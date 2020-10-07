fbc5d00b1c32ae2f06ed412b16f4e99d9abcab21
mateusoro <mateusoro@gmail.com>
2020-10-07 18:54:14 +0000

    d

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-var dataset_links = [];
   
 var url = require('url');
 var magnet = require("magnet-uri");
 const cheerio = require('cheerio');
-var request = require('sync-request');
+var request = require('then-request');
 var express = require('express');
+const Magnet2torrent = require('magnet2torrent-js');
+
+// https://github.com/ngosang/trackerslist
+const trackers = [
+    'udp://tracker.coppersurfer.tk:6969/announce',
+    'udp://tracker.open-internet.nl:6969/announce',
+    'udp://tracker.leechers-paradise.org:6969/announce'    
+];
+
+
+const m2t = new Magnet2torrent({
+    trackers,
+    addTrackersToTorrent: true
+});
+
 
 lista('https://ondeeubaixo.net/-',1,2)
 
@@ -14,49 +28,55 @@ function lista(tipo, ini, tamanho) {
 
         var p = tipo + i;
         l(p);
-        var res = request('GET', p);
-        var data = res.getBody('utf8');
-        carregar_links(data);
+        var res = request('GET', p).done(function (res) {
+          var data = res.getBody('utf8');
+          const $ = cheerio.load(data);
+          var titulos = $('.list-inline > li');
+          titulos.each(function (index, elem) {
+
+              var link = $(this).find('a')[0].attribs.href;
+              var dublado = $(this).find('.idioma_lista').text().trim();
+
+              if (dublado == "Dublado") {
+                  l(link);                  
+                  retorno_carregar_links(link);
+              }
+          });          
+        });
+        
     }
-    retorno_carregar_links();
-   
 
 }
-function carregar_links(data) {
 
-//append(data[1]);
+function retorno_carregar_links(p) {
 
-    const $ = cheerio.load(data);
-
-    var titulos = $('.list-inline > li');
-
-    titulos.each(function (index, elem) {
-
-        var link = $(this).find('a')[0].attribs.href;
-        var dublado = $(this).find('.idioma_lista').text().trim();
-
-        if (dublado == "Dublado") {
-            l(link);
-            dataset_links.push(link);
-        }
-    });
-}
-function retorno_carregar_links() {
-
-    l('retorno_carregar_links');
-    for (var n = 1; n < dataset_links.length; n++) {
-
-        var p = dataset_links[n];
-        var res = request('GET', p);
+      var res = request('GET', p).done(function (res) {
         const $ = cheerio.load(res.getBody('utf8'));      
         $("a[href*='magnet:?xt=urn']").each(function (index, elem) {
             
-            var mag = this.attribs.href;
-            var parsed = magnet(mag);
-            l(parsed.infoHash);       
+            try{
+                var mag = this.attribs.href;
+              
+                var parsed = magnet(mag);
+                m2t.getTorrent(mag).then(torrent => {
+                    var arquivos = {}
+                    for(var a in torrent.files){
+                      arquivos[a] = torrent.files[a].name;
+                    }
+                    l(torrent.infoHash);       
+                    l(arquivos);
+
+                }).catch(e => {
+                    // Timeout or error occured
+                    console.error(e);
+                });
+            }catch(e){
+              l(mag);
+              console.log('Erro')
+            }
 
         });
-    }
+    });
 
 }
 function l(log) {
