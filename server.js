46aa244d5210333fc673c3b0fb901b1c97b25106
mateusoro <mateusoro@gmail.com>
2020-10-09 18:46:49 +0000

    w

--- server.js
+++ server.js
@@ -20,11 +20,6 @@ const trackers = [
     'udp://tracker.open-internet.nl:6969/announce',
     'udp://tracker.leechers-paradise.org:6969/announce'    
 ];
-const m2t = new Magnet2torrent({
-                    trackers,
-                    addTrackersToTorrent: true,
-                    timeout: 20 
-                });
 
 var quantidade = 0;
 
@@ -54,7 +49,7 @@ function lista(tipo, ini, tamanho) {
     }
 
 }
-
+var encontrado = 0;
 function retorno_carregar_links(p) {
 
       var res = request('GET', p).done(function (res) {
@@ -65,11 +60,13 @@ function retorno_carregar_links(p) {
                 var mag = this.attribs.href;                
                 var parsed = magnet(mag);                        
                
-				sleep(1000);
-				if(quantidade > 10){								
-					sleep(2000);
+				if(quantidade > 10){
+					l('Esperando: Fila de Torrent');
 				}else{
-					sleep(200);					
+					sleep(200);
+				}
+				while(quantidade > 10){								
+					sleep(1000);
 				}
 				
 				db.find({ hash: parsed.infoHash }, function (err, docs) {
@@ -78,12 +75,9 @@ function retorno_carregar_links(p) {
 					  }else{
 						  //console.log(docs);
 						  if(docs.length > 0){
-							 l('Já Encontrado: '+parsed.infoHash);
-					      }else{
-							  while(quantidade > 10){
-								l('Esperando: '+quantidade);
-								sleep(1000);
-							  }
+							  encontrado++;
+							 l('Já Encontrado:'+encontrado+' '+parsed.infoHash);
+					      }else{							  
 							  add_torrent(mag);
 						  }
 					  }
@@ -101,14 +95,15 @@ function retorno_carregar_links(p) {
 
 }
 var liberado = 0;
-function add_torrent(mag){
+function add_torrent(mag, time=20){
     
   	quantidade++;
 	const m2t = new Magnet2torrent({
 					trackers,
 					addTrackersToTorrent: true,
-					timeout: 20 
+					timeout: time 
 				});
+	
   	m2t.getTorrent(mag).then(torrent => {
       
       var arquivos = {}
@@ -129,9 +124,78 @@ function add_torrent(mag){
 
   	}).catch(e => {
       // Timeout or error occured
+      	quantidade--;
+		var parsed = magnet(mag);    
+      	console.error(e+" "+time+" "+parsed.infoHash);
+	  	if(time==20){
+			db.find({ hash: parsed.infoHash }, function (err, docs) {
+  					if(err){
+						  console.log(err);
+					  }else{
+						  //console.log(docs);
+						  if(docs.length > 0){
+							  encontrado++;
+							 l('Já Encontrado:'+encontrado+' '+parsed.infoHash);
+					      }else{							  
+							  add_torrent(mag, 60);
+						  }
+					  }
+			});
+		}
+	  
+	});
+    
+
+
+}
+function add_hash(hash, time=20){
+    
+  	quantidade++;
+	const m2t = new Magnet2torrent({
+					trackers,
+					addTrackersToTorrent: true,
+					timeout: time 
+				});
+	
+  	m2t.getTorrent("magnet:?xt=urn:btih:"+hash).then(torrent => {
+      
+      var arquivos = {}
+      for(var a in torrent.files){
+        arquivos[a] = torrent.files[a].name;
+      }
+      liberado++;     
+	  db.insert({hash:torrent.infoHash, arquivos: stringifyObject(arquivos) }, function (err, newDoc) {   
+		  if(err){
+			console.log(err);
+		  }else{
+			l('Liberado: '+liberado+" "+torrent.infoHash);  
+		  }
+		});
+      
+      //l(arquivos);
       quantidade--;
-      console.error(e);
-  	});
+
+  	}).catch(e => {
+      // Timeout or error occured
+      	quantidade--;		  
+      	console.error(e+" "+time+" "+hash);
+	  	if(time==20){
+			db.find({ hash: hash }, function (err, docs) {
+  					if(err){
+						  console.log(err);
+					  }else{
+						  //console.log(docs);
+						  if(docs.length > 0){
+							  encontrado++;
+							 l('Já Encontrado:'+encontrado+' '+hash);
+					      }else{							  
+							  add_hash(hash, 60);
+						  }
+					  }
+			});
+		}
+	  
+	});
     
 
 
@@ -143,10 +207,14 @@ app.get('/carregado', function(req, res){
 		  }else{
 			  //console.log(docs);
 			  if(docs.length > 0){
-				 var text1 = stringifyObject(docs[0]);	
-				  text1 = text1.replace('\n','').replace('\\\'','"')
+				  l(docs[0].arquivos);
+				  var text1 = stringifyObject(docs[0].arquivos,{
+																	indent: '  ',
+																	singleQuotes: false
+																});
 				 res.send("<html>"+text1+"</html>"); 
 			  }else{
+				 add_hash(req.query.hash);
 				 res.send("<html>Não Carregado</html>");	
 			  }
 		  }
@@ -155,9 +223,16 @@ app.get('/carregado', function(req, res){
 app.get('/', function(req, res){	
 	
 	res.send("<html>Rodando</html>");
-	lista('https://ondeeubaixo.net/-',1,2)
+	lista('https://ondeeubaixo.net/-',1,5)
 	
 });
+app.get('/lancamentos', function(req, res){	
+	
+	res.send("<html>Rodando</html>");
+	lista('https://ondeeubaixo.net/lancamentos-',1,4)
+	
+});
+
 
 function l(log) {
     console.log(log);
